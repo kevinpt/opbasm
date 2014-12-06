aaed6a1dbe8b07b1da40734d6a4a72993e252d0c
Kevin <kevinpt@yahoo.com>
2014-12-06 13:33:08 -0500

    Add pb_update mem layout objects

--- pb_update.py
+++ pb_update.py
@@ -29,6 +29,8 @@ from __future__ import print_function, division
 import sys
 import os
 import re
+import math
+import copy
 from optparse import OptionParser
 from subprocess import check_call, CalledProcessError
 
@@ -127,14 +129,14 @@ def main():
             report_error('Named RAM instance does not exist ({})'.format(options.ram_inst))
             print(' Available RAM instances:', file=sys.stderr)
             for n in inames:
-                print('  ', n, file=sys.stderr)
+              print('  {}  [{} {}]'.format(n, ram_insts[n].primitive, ram_insts[n].dimensions), file=sys.stderr)
             sys.exit(1)
 
     # Ask user to select RAMB16 instance
     if prompt_user:
         print('Available RAM instances:')
         for i, n in enumerate(inames):
-            print('  {}) {}'.format(i+1, n))
+            print('  {}) {}  [{} {}]'.format(i+1, n, ram_insts[n].primitive, ram_insts[n].dimensions))
         try:
             sel = int(raw_input('\nSelect RAM instance: ')) - 1
         except ValueError:
@@ -143,7 +145,17 @@ def main():
         if sel < 0 or sel >= len(inames):
             report_error('Invalid selection', exit=1)
 
-    print('\nSelected instance:', inames[sel], 'placed at', ram_insts[inames[sel]])
+    print('\nSelected instance:', inames[sel], 'placed at', ram_insts[inames[sel]].loc)
+
+
+    # Setup a memory layout
+    # FIXME: In the future add support for multiple BRAM selection
+    lo = MemLayout()
+    lo.add_row(MemRow(0, ram_insts[inames[sel]].depth - 1, [ram_insts[inames[sel]]]))
+
+    print('\nMemory layout:')
+    print('\n'.join(lo.summary(4)))
+    print()
 
     # Generate BMM file
     bmm_template = '''ADDRESS_SPACE pb_rom RAMB18 INDEX_ADDRESSING [0x00000000:0x000003FF]
@@ -154,7 +166,7 @@ END_ADDRESS_SPACE;
 '''
 
     bmm_template = bmm_template.replace('<inst>', inames[sel])
-    bmm_template = bmm_template.replace('<loc>', ram_insts[inames[sel]])
+    bmm_template = bmm_template.replace('<loc>', ram_insts[inames[sel]].loc)
 
     with open(bmm_file, 'w') as fh:
         fh.write(bmm_template)
@@ -174,20 +186,147 @@ END_ADDRESS_SPACE;
     sys.exit(0)
 
 
-inst_re = re.compile('inst "([^"]+)" "RAMB\d+.*",.+RAMB\d+_(\w+) *,')
+class Bram(object):
+  '''Keep track of properties of BRAM instances'''
+  def __init__(self, instance, primitive, loc, depth=None, width=None):
+    self.instance = instance
+    self.primitive = primitive
+    self.loc = loc
+    self.depth = depth
+    self.width = width
+    self.lsb = 0
+
+  @property
+  def dimensions(self):
+    return '{}x{}'.format(self.depth if self.depth else '??', self.width if self.width else '??')
+
+
+inst_re = re.compile(r'inst "([^"]+)" "(RAMB\d+.*)",.+RAMB\d+_(\w+) *,')
+
+# The XDL file has different BRAM attributes depending on the device family
+# We need to extract the depth and width of the memories in different ways
+cfg_s3_re = re.compile(r'PORTA_ATTR::(\d+)X(\d+)')
+cfg_s6_re = re.compile(r'DATA_WIDTH_A::(\d+)')
+cfg_v7_re = re.compile(r'READ_WIDTH_A::(\d+)')
 
 def find_ram_instances(xdl_file):
     with open(xdl_file, 'r') as fh:
         lines = fh.readlines()
 
     ram_insts = {}
+    last_bram = None
     for l in lines:
-        m = inst_re.match(l)
+      if last_bram is not None:
+        primitive = ram_insts[last_bram].primitive
+
+        # Spartan-3
+        m = cfg_s3_re.search(l)
         if m:
-            ram_insts[m.group(1)] = m.group(2)
+          ram_insts[last_bram].depth = m.group(1)
+          ram_insts[last_bram].width = m.group(2)
+          last_bram = None
+
+        # Spartan-6
+        m = cfg_s6_re.search(l)
+        if m:
+          width = int(m.group(1))
+          width_np = 2**int(math.log(width, 2)) # Remove any parity bits from width
+          if '16' in primitive:
+            depth = 16384 // width_np
+          elif '8' in primitive:
+            depth = 8192 // width_np
+          else:
+            depth = None
+
+          ram_insts[last_bram].depth = depth
+          ram_insts[last_bram].width = width
+          last_bram = None
+
+        # 7-series
+        m = cfg_v7_re.search(l)
+        if m:
+          width = int(m.group(1))
+          width_np = 2**int(math.log(width, 2)) # Remove any parity bits from width
+          if '36' in primitive:
+            depth = 32768 // width_np
+          elif '18' in primitive:
+            depth = 16384 // width_np
+          else:
+            depth = None
+
+          ram_insts[last_bram].depth = depth
+          ram_insts[last_bram].width = width
+          last_bram = None
+          
+      m = inst_re.match(l)
+      if m:
+          ram_insts[m.group(1)] = Bram(m.group(1), m.group(2), m.group(3))
+          last_bram = m.group(1)
 
     return ram_insts
 
+
+class MemRow(object):
+  def __init__(self, start, end, brams=None):
+    self.start = start
+    self.end = end
+    self.brams = brams if brams else []
+    self.target_width = 18
+
+    lsb = 0
+    for b in reversed(self.brams):
+      b.lsb = lsb
+      lsb += b.width
+
+    if self.target_width > 0 and len(self.brams) > 0:
+      lsb_shift = self.target_width - self.brams[0].width
+      for b in self.brams:
+        b.lsb += lsb_shift
+
+  @property
+  def depth(self):
+    return self.end - self.start + 1
+
+  @property
+  def width(self):
+    return sum(b.width for b in self.brams)
+
+  def add_bram(self, b):
+    self.brams.append(b)
+    if len(self.brams) >= 2:
+      b.lsb = self.brams[-2].lsb - b.width
+    else: # First BRAM in row
+      b.lsb = self.target_width - b.width
+
+  def summary(self, indent=0):
+    return ['{}{:20}  {:7}  {:2} - {:<2}'.format(' '*indent, b.instance, b.dimensions, \
+            b.lsb + b.width-1, b.lsb) for b in self.brams]
+
+  def map(self):
+    m = ''.join('[{}]'.format(b.width) for b in self.brams)
+    if self.width == self.target_width:
+      m = success(m)
+    else:
+      m = m + error('(?)')
+
+    return m
+
+
+class MemLayout(object):
+  def __init__(self):
+    self.rows = []
+
+  def add_row(self, row, relative_start=True):
+    if relative_start and len(self.rows) > 0:
+      row.start = row.start + self.rows[-1].end + 1
+      row.end = row.end + self.rows[-1].end + 1
+    self.rows.append(row)
+
+  def summary(self, indent=0):
+    for i, r in enumerate(self.rows):
+      yield '{}Row {}:  {:4} - {:<4}    {}'.format(' '*indent, i, r.start, r.end, r.map())
+      for s in r.summary(indent + 2):
+        yield s
 
 if __name__ == '__main__':
     main()
