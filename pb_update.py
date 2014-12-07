46991a1a3d79018a3f92308340739cf3b7d5015e
Kevin <kevinpt@yahoo.com>
2014-12-06 20:41:42 -0500

    New multi-BRAM support in pb_update

--- pb_update.py
+++ pb_update.py
@@ -31,6 +31,7 @@ import os
 import re
 import math
 import copy
+import itertools
 from optparse import OptionParser
 from subprocess import check_call, CalledProcessError
 
@@ -108,73 +109,130 @@ def main():
     if not os.path.exists(xdl_file):
         report_error('XDL file not generated', exit=1)
 
-    # Find RAMB16 instances in XDL file
+    # Find BRAM instances in XDL file
     ram_insts = find_ram_instances(xdl_file)
-    inames = ram_insts.keys()
+    inames = sorted(ram_insts.keys())
 
     if len(inames) == 0:
         report_error('No RAM instances found', exit=1)
 
     prompt_user = True
+
+    lo = MemLayout()
+    
     if len(inames) == 1: # Don't bother with prompt if only one RAM is in the netlist
-      sel = 0
+      lo.add_row(MemRow(0,0))
+      lo.rows[0].add_bram(copy.deepcopy(ram_insts[inames[0]]))
+      prompt_user = False
+    elif options.ram_inst is not None: # Check if named instance(s) exists
+      spec = [r.split(',') for r in options.ram_inst.split(':')]
+      spec_flat = [i for s in spec for i in s]
+
+      def show_instances(inames, ram_insts):
+        print(' Available RAM instances:', file=sys.stderr)
+        for n in inames:
+          print('  {}  [{} {}]'.format(n, ram_insts[n].primitive, ram_insts[n].dimensions), file=sys.stderr)
+        sys.exit(1)
+
+      # Check for duplicate instances in the spec
+      if len(set(spec_flat)) < len(spec_flat):
+        report_error('Duplicate instances in BRAM spec')
+        show_instances(inames, ram_insts)
+
+      # Verify all instances in the spec are in the netlist
+      for i in spec_flat:
+        if i not in inames:
+          report_error('Instance does not exist in netlist ({})'.format(i))
+          show_instances(inames, ram_insts)
+
+      # Build a layout from the specification
+      for r in spec:
+        lo.add_row(MemRow(0,0))
+        for b in r:
+          lo.rows[-1].add_bram(copy.deepcopy(ram_insts[b]))
+
       prompt_user = False
 
-    # Check if named instance exists
-    if options.ram_inst is not None:
-        if options.ram_inst in inames:
-            sel = inames.index(options.ram_inst)
-            prompt_user = False
-        else:
-            report_error('Named RAM instance does not exist ({})'.format(options.ram_inst))
-            print(' Available RAM instances:', file=sys.stderr)
-            for n in inames:
-              print('  {}  [{} {}]'.format(n, ram_insts[n].primitive, ram_insts[n].dimensions), file=sys.stderr)
-            sys.exit(1)
 
-    # Ask user to select RAMB16 instance
-    if prompt_user:
-        print('Available RAM instances:')
-        for i, n in enumerate(inames):
-            print('  {}) {}  [{} {}]'.format(i+1, n, ram_insts[n].primitive, ram_insts[n].dimensions))
-        try:
-            sel = int(raw_input('\nSelect RAM instance: ')) - 1
-        except ValueError:
-            sel = -1
+    # Read the mem file
+    mem_data = []
+    with open(options.mem_file, 'r') as fh:
+      mem_data = [l.strip() for l in fh.readlines()]
 
-        if sel < 0 or sel >= len(inames):
-            report_error('Invalid selection', exit=1)
+    if len(mem_data) > 1: # Remove first line with address offset
+      mem_data = mem_data[1:]
 
-    print('\nSelected instance:', inames[sel], 'placed at', ram_insts[inames[sel]].loc)
+    print('Required memory depth:', len(mem_data))
+
+    # Interactive selection of BRAM blocks to build memory layout
+    target_width = 18
+    target_depth = len(mem_data)
+    if prompt_user: lo.add_row(MemRow(0, 0)) # Start with a default, empty row
+
+    while prompt_user and not lo.valid(target_depth, target_width):
+      if len(lo.rows[0].brams) > 0:
+        # Show current layout
+        print('\nMemory layout:')
+        print('\n'.join(lo.summary(4)))
+
+      if len(inames) == 0:
+        report_error('No BRAMs remaining', exit=1)
+
+      # Prompt user
+      print('\nAvailable RAM instances:')
+      for i, n in enumerate(inames):
+        print('  {}) {}  [{} {}]'.format(i+1, n, ram_insts[n].primitive, ram_insts[n].dimensions))
+      print('  q) Quit')
+      sel = raw_input('\nSelect RAM instance: ')
+      if sel.lower() == 'q': sys.exit(0)
+
+      try:
+        sel = int(sel) - 1
+      except ValueError:
+        sel = -1
+
+      if sel < 0 or sel >= len(inames):
+        report_error('Invalid selection', exit=1)
+
+      lo.rows[-1].add_bram(copy.deepcopy(ram_insts[inames[sel]]))
+      if lo.rows[-1].width > target_width:
+        report_error('Row is too wide ({} bits)'.format(lo.rows[-1].width), exit=1)
+
+      if lo.rows[-1].valid(target_width) and not lo.valid(target_depth, target_width):
+        # Row is complete but depth is not satisfied
+        lo.add_row(MemRow(0, 0))
+
+      # Remove selected BRAM from available list
+      inames.remove(inames[sel])
 
 
-    # Setup a memory layout
-    # FIXME: In the future add support for multiple BRAM selection
-    lo = MemLayout()
-    lo.add_row(MemRow(0, ram_insts[inames[sel]].depth - 1, [ram_insts[inames[sel]]]))
 
-    print('\nMemory layout:')
+    # Show final layout
+    print('\nFinal memory layout:')
     print('\n'.join(lo.summary(4)))
     print()
 
+    # Make sure all BRAMs in layout have the same width
+    widths = [b.width for r in lo.rows for b in r.brams]
+    if len(widths) > 0:
+      if not all(widths[0] == w for w in widths[1:]):
+        report_error('Mixed width BRAMs not supported in memory layout', exit=1)
+
+    # Make sure the layout depth is correct
+    if lo.rows[-1].end + 1 != target_depth:
+      report_error('Memory layout depth does not match required {} words'.format(target_depth), exit=1)
+
+    print('Instances: "{}"'.format(lo.instance_spec))
+
     # Generate BMM file
-    bmm_template = '''ADDRESS_SPACE pb_rom RAMB18 INDEX_ADDRESSING [0x00000000:0x000003FF]
-    BUS_BLOCK
-        <inst> [17:0] PLACED = <loc>;
-    END_BUS_BLOCK;
-END_ADDRESS_SPACE;
-'''
-
-    bmm_template = bmm_template.replace('<inst>', inames[sel])
-    bmm_template = bmm_template.replace('<loc>', ram_insts[inames[sel]].loc)
-
     with open(bmm_file, 'w') as fh:
-        fh.write(bmm_template)
+        fh.write(lo.bmm)
+
 
     # Run data2mem
-    print('Running data2mem...')
+    print('\nRunning data2mem...')
     d2m_cmd = ['data2mem', '-bm', bmm_file, '-bd', options.mem_file, \
-               '-bt', bit_file, '-o', 'b', options.out_bit_file]
+               '-bt', bit_file, '-bx', '.','-o', 'b', options.out_bit_file]
     print(' ', ' '.join(d2m_cmd))
     try:
       check_call(d2m_cmd)
@@ -195,6 +253,10 @@ class Bram(object):
     self.depth = depth
     self.width = width
     self.lsb = 0
+
+  @property
+  def msb(self):
+    return self.lsb + self.width - 1
 
   @property
   def dimensions(self):
@@ -297,6 +359,16 @@ class MemRow(object):
       b.lsb = self.brams[-2].lsb - b.width
     else: # First BRAM in row
       b.lsb = self.target_width - b.width
+      self.end = self.start + b.depth - 1
+
+  def valid(self, width):
+    return True if self.width == width else False
+
+  # FIXME: Remove OUTPUT parameter from bit lane def.
+  @property
+  def bus_block(self):
+    lanes = ['    {} [{}:{}] LOC = {} OUTPUT = {}.mem;'.format(b.instance, b.msb, b.lsb, b.loc, b.loc) for b in self.brams]
+    return '  BUS_BLOCK\n' + '\n'.join(lanes) + '\n  END_BUS_BLOCK;'
 
   def summary(self, indent=0):
     return ['{}{:20}  {:7}  {:2} - {:<2}'.format(' '*indent, b.instance, b.dimensions, \
@@ -316,11 +388,41 @@ class MemLayout(object):
   def __init__(self):
     self.rows = []
 
+  @property
+  def bmm(self):
+    first_bram = self.rows[0].brams[0]
+    size = first_bram.depth * first_bram.width
+    btype = 'unknown'
+    if size <= 1024*16:
+      btype = 'RAMB16'
+    elif size <= 1024*18:
+      btype = 'RAMB18'
+    elif size <= 1024*32:
+      btype = 'RAMB32'
+    elif size <= 1024*36:
+      btype = 'RAMB36'
+
+
+    blocks = [r.bus_block for r in self.rows]
+    # data2mem has some goofy way of counting "words" that changes depending on how
+    # many BRAMs are in a row
+    end_addr = (self.rows[-1].end+1)*len(self.rows[0].brams)-1
+    return 'ADDRESS_SPACE pb_rom {} INDEX_ADDRESSING [0x{:08X}:0x{:08X}]\n'.format(btype, 0, end_addr) + \
+            '\n'.join(blocks) + '\nEND_ADDRESS_SPACE;'
+
+  @property
+  def instance_spec(self):
+    return ':'.join(','.join(b.instance for b in r.brams) for r in self.rows)
+
   def add_row(self, row, relative_start=True):
     if relative_start and len(self.rows) > 0:
       row.start = row.start + self.rows[-1].end + 1
       row.end = row.end + self.rows[-1].end + 1
     self.rows.append(row)
+
+  def valid(self, depth, width):
+    rv = all(r.valid(width) for r in self.rows)
+    return True if rv and sum(r.depth for r in self.rows) == depth else False
 
   def summary(self, indent=0):
     for i, r in enumerate(self.rows):
