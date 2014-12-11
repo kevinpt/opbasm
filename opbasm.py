3259cd987b875ba8798a144d3135da7fd4294b8d
Kevin <kevinpt@yahoo.com>
2014-12-10 20:30:34 -0500

    Mark all INST directives with keep_auto

--- opbasm.py
+++ opbasm.py
@@ -1277,6 +1277,11 @@ class Assembler(object):
 
         elif s.is_instruction(): cur_label = None
 
+    # Apply keep_auto to INST directives
+    for s in instructions:
+      if s.command == 'inst' and 'keep' not in s.tags:
+        s.tags['keep_auto'] = (True,)
+
 
     # Create default jump instruction
     if self.default_jump is None:
