00650cc2d367cead50de4812c5fc52f923047563
Kevin Thibedeau <kevin.thibedeau@gmail.com>
2017-10-07 23:18:40 -0400

    Hosting all scripts locally

--- doc/conf.py
+++ doc/conf.py
@@ -31,8 +31,7 @@ import sys
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.mathjax',
-    'sphinx.ext.githubpages']
+extensions = ['sphinx.ext.githubpages']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
